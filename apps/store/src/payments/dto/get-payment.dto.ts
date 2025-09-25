f62b85d7250f3af05ec9c289e4603ce3006d572c
Jacob Julius <jeccopatrick@outlook.com>
2025-09-25 08:58:25 +0300

    feat: add pagination

---
+++ apps/store/src/payments/dto/get-payment.dto.ts
@@ -0,0 +1,61 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
+import {
+  IsDateString,
+  IsEnum,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { AbstractGetDto, PaymentStatus } from '@app/common';
+import { GetOrderDto } from '../../orders/dto/get-order.dto';
+
+export class GetPaymentDto extends AbstractGetDto {
+  @ApiProperty({
+    example: '1',
+    required: true,
+  })
+  @IsNumber()
+  @IsOptional()
+  @Expose()
+  id?: number;
+
+  @ApiProperty({
+    example: 49,
+    required: true,
+  })
+  @IsNumber()
+  @Expose()
+  amount?: number;
+
+  @ApiProperty({
+    type: Date,
+    required: true,
+  })
+  @IsDateString()
+  @Expose()
+  paid_date?: Date;
+
+  @ApiProperty({
+    enum: PaymentStatus,
+    default: PaymentStatus.PENDING,
+    required: true,
+  })
+  @IsEnum(PaymentStatus)
+  @Expose()
+  payment_status?: PaymentStatus;
+
+  @IsObject()
+  @Type(() => GetOrderDto)
+  @Expose()
+  order?: GetOrderDto;
+
+  @ApiProperty({
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  @Expose()
+  note?: string;
+}
