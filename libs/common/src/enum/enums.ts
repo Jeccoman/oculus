52b30e1bcca5c5c2a4e76133a7b05268acc5fdb1
Jacob Julius <jeccopatrick@outlook.com>
2025-09-25 08:38:42 +0300

    feat: add payment and store

---
+++ libs/common/src/enum/enums.ts
@@ -0,0 +1,13 @@
+export enum OrderStatus {
+  PENDING = 'pending',
+  PAID = 'paid',
+  PROCESSING = 'processing',
+  COMPLETED = 'completed',
+  CANCELLED = 'cancelled',
+}
+
+export enum PaymentStatus {
+  PENDING = 'pending',
+  FAILED = 'failed',
+  PAID = 'paid',
+}
