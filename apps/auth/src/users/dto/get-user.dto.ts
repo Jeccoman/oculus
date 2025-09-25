5062c7aa4bb81553846659dec8eba86583d110de
Jacob Julius <jeccopatrick@outlook.com>
2025-09-25 14:33:53 +0300

    initial commit

---
+++ apps/auth/src/users/dto/get-user.dto.ts
@@ -0,0 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
+import {
+  IsArray,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { GetAccessDto } from '../../accesses/dto/get-access.dto';
+import { AbstractGetDto } from '@app/common';
+
+export class GetUserDto extends AbstractGetDto {
+  @ApiProperty({
+    example: '1',
+    required: true,
+  })
+  @IsNumber()
+  @IsOptional()
+  @Expose()
+  id?: number;
+
+  @ApiProperty({
+    example: 'mahdad.ghasemian@gmail.com',
+    required: true,
+  })
+  @IsEmail()
+  @IsOptional()
+  @Expose()
+  email?: string;
+
+  @ApiProperty({
+    example: 'Mahdad Ghasemian',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  @Expose()
+  full_name?: string;
+
+  @ApiProperty({
+    type: GetAccessDto,
+    required: true,
+    isArray: true,
+  })
+  @IsArray()
+  @IsNotEmpty({ each: true })
+  @Type(() => GetAccessDto)
+  @Expose()
+  accesses?: GetAccessDto[];
+}
