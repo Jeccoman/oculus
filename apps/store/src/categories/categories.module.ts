1623e0f74970803086c1dfb651329d47f4eae498
Jacob Julius <jeccopatrick@outlook.com>
2025-09-27 14:15:02 +0300

    initial commit

---
+++ apps/store/src/categories/categories.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CategoriesController } from './categories.controller';
+import { CategoriesRepository } from './categories.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { Category } from '../libs';
+
+@Module({
+  imports: [ConfigModule, TypeOrmModule.forFeature([Category])],
+  controllers: [CategoriesController],
+  providers: [CategoriesService, CategoriesRepository],
+  exports: [CategoriesService],
+})
+export class CategoriesModule {}
