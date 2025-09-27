1dd5995c9b5aec93c6dd8ed96076126083970281
Jacob Julius <jeccopatrick@outlook.com>
2025-09-27 14:15:22 +0300

    initial commit

---
+++ apps/store/src/categories/dto/create-category.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, IsUrl } from 'class-validator';
+
+export class CreateCategoryDto {
+  @ApiProperty({
+    example: 'Category 1',
+    required: true,
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({
+    example: 'http://www.localhost/image1000.jpg',
+    required: false,
+  })
+  @IsString()
+  @IsUrl()
+  @IsOptional()
+  image?: string;
+}
